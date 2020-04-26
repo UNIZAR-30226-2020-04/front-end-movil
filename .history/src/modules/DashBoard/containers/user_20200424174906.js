cf8be7a952e66c1e494f596a70001a4cac372afc
ediazl <55806521+ediazl@users.noreply.github.com>
2020-04-26 13:43:48 +0200

    perfil usuario ok

---
+++ .history/src/modules/DashBoard/containers/user_20200424174906.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { StyleSheet} from 'react-native';
+
+//Componente para almacenar informacion del usuario en el state
+export default class User extends Component{
+
+    constructor(props) {
+        super(props);
+        user={
+          email:"",
+          fnacimiento:"",
+          foto:"",
+          nick:"asdi",
+          username:"",
+          passWord:"",
+          checkPassword:"",
+          newUsername:"",
+        }
+    }
+
+    
+  }
---
+++ .history/src/modules/DashBoard/containers/user_20200424175733.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet} from 'react-native';
+
+//Componente para almacenar informacion del usuario en el state
+export default class User extends Component{
+
+    constructor(props) {
+        super(props);
+        user={
+          
+        }
+    }
+
+    
+  }
