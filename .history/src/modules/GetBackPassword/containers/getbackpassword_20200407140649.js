dcd54f4fe1c888af75758de8e1ed95c35871e654
HERNANDEZ GRACIA, JOSE IGNACIO <740491@unizar.es>
2020-05-06 11:23:38 +0200

    prueba vaciado rama

--- .history/src/modules/GetBackPassword/containers/getbackpassword_20200407140649.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert, ScrollView, Image, ImageBackground, TouchableOpacity } from 'react-native';
-
-
-export default class GetBackPassword extends Component{
-
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# front-end-movil
-Repositorio de front-end MÓVIL. Aplicación reproductor de música. Grupo 4 - Proyecto Software - 2020
